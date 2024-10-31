da1ee1417ed268f075174038364294f6108e149f
Chris Burke <burke1791@gmail.com>
2024-10-31 17:41:23 -0500

    ai in progress

--- include/ai.h
+++ include/ai.h
@@ -3,6 +3,23 @@
 
 #include "game.h"
 
+typedef struct State {
+  Board *board;
+  Piece nextTurn;
+  int visitCount;
+  double winScore;
+} State;
+
+typedef struct Node {
+  Node *parent;
+  State *state;
+  Node **children;
+} Node;
+
+typedef struct Tree {
+  Node *root;
+} Tree;
+
 int next_move(Game *g);
 
 #endif /* AI_H */
--- src/ai.c
+++ src/ai.c
@@ -2,6 +2,16 @@
 
 #include "ai.h"
 
+#define MAX_ITERATIONS 100
+
+Tree *new_tree();
+Node *new_node(Node *parent, Board *src);
+State *new_state(Board *src);
+
+void destroy_tree(Tree *t);
+
+void copy_board(Board *src, Board *tgt);
+
 /**
  * @brief runs the AI logic and returns the position of the square
  *        where the next move should be made
@@ -16,4 +26,58 @@ int next_move(Game *g) {
   }
 
   return -1;
+}
+
+Tree *new_tree(Game *g) {
+  Tree *t = malloc(sizeof(Tree));
+
+  Node *root = new_node(NULL, g->board);
+}
+
+Node *new_node(Node *parent, Board *src) {
+  Node *n = malloc(sizeof(Node));
+  n->parent = parent;
+  n->state = new_state(src);
+  n->children = NULL;
+
+  return n;
+}
+
+static Piece get_next_turn(Board *b) {
+  int numX = 0;
+  int numO = 0;
+
+  for (int i = 0; i < 9; i++) {
+    if (b->squares[i]->piece == PIECE_X) numX++;
+    if (b->squares[i]->piece == PIECE_O) numO++;
+  }
+
+  if (numO >= numX) {
+    return PIECE_X;
+  } else {
+    return PIECE_O;
+  }
+}
+
+State *new_state(Board *src) {
+  State *s = malloc(sizeof(State));
+  s->board = new_board();
+  copy_board(src, s->board);
+
+  s->nextTurn = get_next_turn(s->board);
+
+  s->visitCount = 0;
+  s->winScore = 0;
+
+  return s;
+}
+
+void copy_board(Board *src, Board *tgt) {
+  for (int i = 0; i < 9; i++) {
+    tgt->squares[i]->piece = src->squares[i]->piece;
+  }
+}
+
+void destory_tree(Tree *t) {
+  
 }
